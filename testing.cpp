a9add45c6b09d7f7fc1b984a0605d8a130136bc6
giministk <irishkau766@gmail.com>
2024-12-17 21:32:42 +0300

    Add files via upload

---
+++ testing.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <cmath>
+
+
+
+float TwoFunk(float x, float y) {
+
+    float c, r, s;
+
+    r = pow(pow(y, 2) + pow(x, 2), (1 / 2)) / x;
+    s = pow(cos((asin(1.0) / 2) * x), 2);
+
+    if (r < s) {
+        c = s;
+    }
+    else {
+        c = r;
+    }
+
+    return c;
+};
+
+int main() {
+    return 0;
+}
