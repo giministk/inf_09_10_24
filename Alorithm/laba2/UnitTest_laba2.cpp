c5e8a791f02a501ab917a868ac6d956fa2353cac
giministk <irishkau766@gmail.com>
2025-05-19 10:58:51 +0300

    commit

---
+++ Alorithm/laba2/UnitTest_laba2.cpp
@@ -0,0 +1,31 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../alg_laba2/alg_laba2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestlaba2
+{
+	TEST_CLASS(UnitTestlaba2)
+	{
+	public:
+		
+		TEST_METHOD(TestMinimal)
+		{
+			double result = simpson_rule(function, 5, 7, 2); // минимальоне четное кол-во сегментов
+			Assert::AreEqual(2.0, result); //почему-то выдвает ошибку если вводить не дробью, хотя тип дабл
+		}
+
+		TEST_METHOD(TestAutoCorrection)
+		{
+			double result = simpson_rule(function, 5, 7, 5); // функция сама меняет нечетное на ближайшее чётное
+			Assert::AreEqual(2.0, result);
+		}
+
+		TEST_METHOD(TestGreatCountOfSegments)
+		{
+			double result = simpson_rule(function, 5, 7, 100); // большое число сегментов
+			Assert::AreEqual(2.0, result);
+		}
+	};
+}
---
+++ Alorithm/laba2/alg_laba2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <cmath>
+#include <locale>
+
+int segments = 10;
+
+double simpson_rule(double (*f)(double), double a, double b, int n) {
+    if (n % 2 == 1) n++; // Делаем n четным
+    double h = (b - a) / n;
+    double sum = f(a) + f(b);
+
+    for (int i = 1; i < n; i += 2)
+        sum += 4 * f(a + i * h);
+
+    for (int i = 2; i < n - 1; i += 2)
+        sum += 2 * f(a + i * h);
+
+    return (h / 3) * sum;
+}
+
+double function(double x) {
+    return 4 - 3; // Интегрируемая функция
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    double result = simpson_rule(function, 5, 7, segments); // 5 - ограничение справа, 7 - ограничение слева
+    std::cout << "Приближенное значение интеграла: " << result << std::endl; // результат выходит точный, тк прямоугольник
+    return 0;
+}
